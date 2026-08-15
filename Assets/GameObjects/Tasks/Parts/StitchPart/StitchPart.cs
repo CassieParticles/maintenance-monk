9f79d6e5e9c71d9c433dc46a771a63ecdfab12c8
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-08-15 18:32:43 +0100

    Removed old code, added new stuff

---
+++ Assets/GameObjects/Tasks/Parts/StitchPart/StitchPart.cs
@@ -0,0 +1,26 @@
+﻿namespace GameObjects.Tasks.Parts.StitchPart
+{
+    public class StitchPart: Part
+    {
+        public override void InitPart()
+        {
+            throw new System.NotImplementedException();
+        }
+        public override void StartPart()
+        {
+            throw new System.NotImplementedException();
+        }
+        public override void FinishPart()
+        {
+            throw new System.NotImplementedException();
+        }
+        public override void CleanupPart()
+        {
+            throw new System.NotImplementedException();
+        }
+        public override float FinalScore()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
