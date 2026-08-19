cb224816c51c649c81e4f4377a2ccad555fe3261
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-08-19 20:36:33 +0100

    Added components for sticker part

---
+++ Assets/GameObjects/Tasks/Parts/DragSticker/DragStickerPart.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+namespace GameObjects.Tasks.Parts.DragSticker
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class DragStickerPart: Part
+    {
+        public override void InitPart()
+        {
+            throw new System.NotImplementedException();
+        }
+        public override void StartPart()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void FixedUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override void FinishPart()
+        {
+            throw new System.NotImplementedException();
+        }
+        public override void CleanupPart()
+        {
+            throw new System.NotImplementedException();
+        }
+        public override float FinalScore()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/GameObjects/Tasks/Parts/DragSticker/StickerCollider.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace GameObjects.Tasks.Parts.DragSticker
+{
+    public class StickerCollider : MonoBehaviour
+    {
+        
+    }
+}
