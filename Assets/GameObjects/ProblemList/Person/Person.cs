429f43fc9177ce95521d7ecbcbf8bf9e74379fcb
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-09-02 15:03:55 +0100

    Person added

---
+++ Assets/GameObjects/ProblemList/Person/Person.cs
@@ -0,0 +1,36 @@
+﻿using GameObjects.Player;
+using GameObjects.ProblemList.Problems;
+using TMPro;
+using UnityEngine;
+
+namespace GameObjects.ProblemList.Person
+{
+    public class Person : MonoBehaviour
+    {
+        [SerializeField]private TextMeshProUGUI chatMessage;
+        
+        private ProblemList _problemList;
+        private Problem _problem;
+        
+        public void SetUpPerson(Problem problem)
+        {
+            _problemList = FindAnyObjectByType<ProblemList>();
+            _problem = problem;
+
+            chatMessage.text = _problem.message;
+
+            PlayerData.Instance.State = PlayerStates.Talking;
+            
+            GetComponent<RectTransform>().anchoredPosition = new Vector2(-557, 133);
+        }
+
+        public void ClosePerson()
+        {
+            _problemList.AddProblem(_problem);
+            
+            PlayerData.Instance.State = PlayerStates.Waiting;
+            
+            Destroy(gameObject);
+        }
+    }
+}
